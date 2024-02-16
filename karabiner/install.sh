ba9af90acd0e4084721edf28be46f5d3142f0b32
Yuthakarn Arngmaneekul <yuthakarn@gmail.com>
2024-02-17 01:05:10 +0700

    add karabiner config

---
+++ karabiner/install.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+ln -s $DOTFILES_ROOT/karabiner/karabiner ~/.config
--- zsh/pyenv-virtualenvwrapper.zsh
+++
@@ -1,4 +0,0 @@
-# For pyenv-virtualenvwrapper
-export VIRTUALENVWRAPPER_PYTHON=$HOMEBREW_PREFIX/bin/python3
-export WORKON_HOME=$HOME/.virtualenvs
-source $HOMEBREW_PREFIX/bin/virtualenvwrapper.sh
